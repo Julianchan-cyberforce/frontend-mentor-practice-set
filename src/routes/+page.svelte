734ba5d27adce23d13b4ed6fa2d575b506235a89
Julian Chan <julianchan@cyberforce.com.hk>
2025-11-14 18:15:41 +0800

    Finished QRcode component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import img from '$lib/assets/image-qr-code.png'
+</script>
+
+<div class="flex justify-center items-center h-screen w-screen">
+  <div class="font-outfit bg-white rounded-xl flex text-center justify-evenly items-center flex-col text-pretty shadow-md w-54">
+    <img src={img} alt="QRCode" class="rounded-2xl p-2">
+    <p class="text-slate-900 font-bold p-2">
+      Improve your front-end skills by building projects
+    </p>
+    <p class="pb-6 px-6 text-xs text-slate-500">
+      Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+    </p>
+  </div>
+</div>
